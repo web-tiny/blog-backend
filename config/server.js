957367eeaccd78694aa158b1b6cc462a0c22153e
焦仁贵 <tiny.jiao@aliyun.com>
2021-04-11 18:13:09 +0800

    init backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '63c48856f7cbc000099f1343b910761e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eae0a545-b023-4547-b283-48acb71aedd5'
+};
