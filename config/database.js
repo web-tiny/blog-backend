c45ea920629e3f3d91c37d968fe5f6c14cd1a094
焦仁贵 <tiny.jiao@aliyun.com>
2021-04-11 21:31:36 +0800

    create database blog

--- config/database.js
+++ config/database.js
@@ -23,7 +23,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'mysql'),
+        database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'jrg123456'),
       },
