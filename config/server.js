17cc354cefd40c4b1bdad80545b6da03bbe47ba6
焦仁贵 <tiny.jiao@aliyun.com>
2021-04-12 14:04:10 +0800

    update port to: 1336

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1336),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '63c48856f7cbc000099f1343b910761e'),
